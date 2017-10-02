51a62e85d8f5d92c0edad68640bd3d3d346045da
alikahwaji <ali.kahwaji@hotmail.com>
2017-10-02 21:20:57 +0400

    Testing and requiring our own notes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ console.log('Starting personal note app project')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes')
 
+var res = notes.addNumbers(5, 10)
+console.log(res)
+// var user =os.userInfo()
 
-var user =os.userInfo()
-
-fs.appendFile('noteAppProject.txt', `Personal project note app by ${user.username}!`)
+// fs.appendFile('noteAppProject.txt', `Personal project note app by ${user.username}! You are ${notes.age}.`)
---
+++ notes.js
@@ -0,0 +1,11 @@
+console.log('Starting our notes')
+
+// module.exports.addNote = () => {
+//     console.log('addNote')
+//     return 'New Note'
+// }
+
+module.exports.addNumbers = (a, b) => {
+    console.log(a+b)
+    return 'The sum of adding'
+}
