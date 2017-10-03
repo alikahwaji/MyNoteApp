772c39ce8987a608120fe1e21194bcc068de3370
alikahwaji <ali.kahwaji@hotmail.com>
2017-10-03 22:26:32 +0400

    testing with lodash

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const os = require('os')
 const _ = require('lodash')
 const notes = require('./notes')
 
-var res = notes.addNumbers()
-console.log(res)
+
+console.log(_.isString(true))
+console.log(_.isString('Ali'))
+// var res = notes.addNumbers()
+// console.log(res)
 // var user =os.userInfo()
 
 // fs.appendFile('noteAppProject.txt', `Personal project note app by ${user.username}! You are ${notes.age}.`)
