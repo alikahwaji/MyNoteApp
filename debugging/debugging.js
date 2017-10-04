9a27df3e6df9305aa48a591058ecebb797f547d8
alikahwaji <ali.kahwaji@hotmail.com>
2017-10-05 00:06:52 +0400

    debugging with nodejs

---
+++ debugging/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'Ali'
+}
+
+person.age = 37
+
+person.name = 'Mike'
+
+console.log(person)
--- notes.js
+++ notes.js
@@ -50,7 +50,8 @@ var removeNote = (title) => {
 }
 
 var logNote = (note) => {
-    console.log('---')
+    debugger
+    console.log('----')
     console.log(`Title ${note.title}`)
     console.log(`Body ${note.body}`)
 }
