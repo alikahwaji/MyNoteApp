2eb2849baa41c908c3d8a13b87b6069dc905b56c
alikahwaji <ali.kahwaji@hotmail.com>
2017-10-04 10:08:25 +0400

    Cleaning some exta code

--- app.js
+++ app.js
@@ -3,18 +3,8 @@ console.log('Starting personal note app project')
 // requiring fs and os modules to create and require our own notes files
 
 const fs = require('fs')
-const os = require('os')
 const _ = require('lodash')
+
 const notes = require('./notes')
 
 
-console.log(_.isString(true))
-console.log(_.isString('Ali'))
-
-var filterArray = _.uniq(['omar'])
-console.log(filterArray)
-// var res = notes.addNumbers()
-// console.log(res)
-// var user =os.userInfo()
-
-// fs.appendFile('noteAppProject.txt', `Personal project note app by ${user.username}! You are ${notes.age}.`)
